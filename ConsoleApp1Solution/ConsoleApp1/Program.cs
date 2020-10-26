c744f0ea14dbfd67e52f65e8a68da84a3a80a5fd
aliia.baimuratova@gmail.com <aliia.baimuratova@gmail.com>
2020-10-26 15:11:02 +0600

    initialcommit

--- ConsoleApp1Solution/ConsoleApp1/Program.cs
+++ ConsoleApp1Solution/ConsoleApp1/Program.cs
@@ -1,12 +1,40 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Security.Cryptography.X509Certificates;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
 
 namespace ConsoleApp1
 {
     class Program
     {
-        static void Main(string[] args)
+        public static async Task Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            HttpClient client = new HttpClient();
+            HttpResponseMessage res = await  client.GetAsync(args[0]);
+            if (res.IsSuccessStatusCode)
+            {
+                string content = await res.Content.ReadAsStringAsync();
+                emails(content);
+                   
+            
+            }
+            //Console.WriteLine("Hello World!");
+
+             void emails(string text)
+            {
+                Regex reg = new Regex(@"[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,6}", RegexOptions.IgnoreCase);
+
+                MatchCollection matches = reg.Matches(text);
+                // Report the number of matches found.
+                int noOfMatches = matches.Count;
+                // Report on each match.
+                foreach (Match match in matches)
+                {
+                    Console.WriteLine(match.Value.ToString());
+                }
+            }
         }
     }
 }
